bfdd4ca2c5bd39b03b37921e95c891857daa5d42
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2018-12-26 22:31:56 -0800

    apps

--- myscript.js
+++ myscript.js
@@ -1,16 +1,9 @@
 console.log('working');
 
 $(document).ready(function() {
-    $("<div id ='nightMode'></div>" ).insertBefore($("body"));
-    $("body").css('transform', 'translateY(50px)');
-    $("#nightMode").append($("<button class='night-button' id='night-button-1'>Nightmode 1 </button>"));
-
-
-    $('#night-button-1').click(function(){
-        $("body").css('background-color','#2f2f30');
-        $('h1, h2, h3, h4, h5, h6, p, a').css('color','white');
-
-    })
+    $("<div id ='nightMode'> Test </div>" ).insertBefore($("body"));
+    $("body").css('transform', 'translateY(30px)');
+    $("#nightMode").append($("<div class = 'button'> <button id = 'button1' type= 'button'> Night1 </button> </div>"));
 });
 
 
